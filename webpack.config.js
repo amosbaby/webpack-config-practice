fbff00696c64bac8c36c37c38a83e9e965f5b0ba
amosbaby <931963081@qq.com>
2022-05-19 21:10:40 +0800

    add css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
 
 module.exports = {
-  mode:'development',
-  entry:'./src/index.js',
+  mode:'development', // 运行模式，可被命令行覆盖
+  entry:'./src/main.css', // 入口文件
   output:{
-    filename:'bundle.js',
-    path:path.join(__dirname,'dist')
+    filename:'bundle.css', // 输出文件名
+    path:path.join(__dirname,'dist') // 输出目录
+  },
+  module:{
+    rules:[{
+      test:/\.css$/, // 匹配所有的css文件,
+      use: 'css-loader' // 对应的css-loader名称
+    }]
   }
 }
