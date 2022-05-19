67c131f280cad687fddc6a2ca2b74f5371c742d2
amosbaby <931963081@qq.com>
2022-05-19 21:22:40 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode:'development', // 运行模式，可被命令行覆盖
-  entry:'./src/main.css', // 入口文件
+  entry:'./src/index.js', // 入口文件
   output:{
-    filename:'bundle.css', // 输出文件名
+    filename:'bundle.js', // 输出文件名
     path:path.join(__dirname,'dist') // 输出目录
   },
   module:{
@@ -12,5 +13,8 @@ module.exports = {
       test:/\.css$/, // 匹配所有的css文件,
       use: 'css-loader' // 对应的css-loader名称
     }]
-  }
+  },
+  plugins:[new HtmlWebpackPlugin({
+    template:'./src/index.html'
+  })]
 }
