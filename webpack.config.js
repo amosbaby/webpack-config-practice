9c90a2344d8fefe3e0bc5bab37bc15faed700344
amosbaby <931963081@qq.com>
2022-05-19 21:45:42 +0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,12 @@ const config = {
     filename:'bundle.js', // 输出文件名
     path:path.join(__dirname,'dist') // 输出目录
   },
+  devServer:{
+    contentBase:path.resolve(__dirname,'public') ,//静态文件目录
+    compress:true, // 是否启动gzip压缩
+    port:8000, // 端口号,
+    open :true // 是否打开浏览器
+  },
   module:{
     rules:[{
       test:/\.css$/, // 匹配所有的css文件,
