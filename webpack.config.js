e71e280b1fc347235222fef3be002383374c03da
amosbaby <931963081@qq.com>
2022-05-20 11:07:55 +0800

    use asset module in webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,18 @@ const config = {
         'sass-loader'] // 对应的css-loader名称
     },{
       test:/\.(jpe?g|png|gif)$/i,
-      use:[
+      type:'asset',
+      generator:{
+        // 输出文件位置以及文件名
+        filename:'[name]_[contenthash:8].[ext]'
+      },
+      parser:{
+        dataUrlCondition:{
+          // 文件小于 50k 会转换为 base64，大于则拷贝文件
+          maxSize: 50 * 1024
+        }
+      }
+      // use:[
         
         // {
         //   loader:'file-loader',
@@ -53,27 +64,38 @@ const config = {
         //   }
         // },
         // 小于limit的就会转成base64，否则回退到file-loader,两者配其一即可
-        {
-          loader:'url-loader',
-          options:{
-            name:'[name]_[contenthash:8].[ext]',
-            // 文件小于 50k 会转换为 base64，大于则拷贝文件
-            limit:50*1024
-          }
-        }
-      ]
+        // {
+        //   loader:'url-loader',
+        //   options:{
+        //     name:'[name]_[contenthash:8].[ext]',
+        //     // 文件小于 50k 会转换为 base64，大于则拷贝文件
+        //     limit:50*1024
+        //   }
+        // }
+      // ]
     },{
       test:/\.(woff2?|eot|ttf|otf)$/i,
-      use:[
-        {
-          loader:'url-loader',
-          options:{
-            // 体积大于 10KB 打包到 fonts 目录下
-            name:'fonts/[name]_[contenthash:8].[ext]',
-            limit:10*1024
-          }
+      type:'asset',
+      generator:{
+        // 输出文件位置以及文件名
+        filename:'[name]_[contenthash:8].[ext]'
+      },
+      parser:{
+        dataUrlCondition:{
+          // 文件小于 10k 会转换为 base64，大于则拷贝文件
+          maxSize: 10 * 1024
         }
-      ]
+      }
+      // use:[
+      //   {
+      //     loader:'url-loader',
+      //     options:{
+      //       // 体积大于 10KB 打包到 fonts 目录下
+      //       name:'fonts/[name]_[contenthash:8].[ext]',
+      //       limit:10*1024
+      //     }
+      //   }
+      // ]
 
     }
     
